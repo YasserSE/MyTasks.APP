9196242ff34edf0ee1ee312d930c254496b5d108
Yasser Santiago <yasser.santiagoe@gmail.com>
2023-04-27 21:33:10 +0200

    Empieza Sprint 3

---
+++ src/components/TaskList.vue
@@ -0,0 +1,95 @@
+<template>
+    <form @keyup.enter="handleCreate" @submit.prevent>
+        <label>New task:</label><input type="text" v-model="taskTitle">
+    </form>
+    <main>
+        <div>
+            <h3>Open Tasks</h3>
+            <div v-for="task in tasks" :key="task.id"  class="taskCard">
+                <p>{{ task.title }}</p>
+                <button>Pending</button>
+            </div>
+        </div>
+        <div>
+            <h3>Completed Tasks</h3>
+            
+        </div>
+    </main>
+</template>
+
+<script setup>
+import { useTaskStore } from '../stores/tasks'
+import { useUserStore } from '../stores/user'
+import { onMounted, ref, computed } from 'vue'
+
+//Variables
+const store = useTaskStore()
+const userStore = useUserStore()
+const taskTitle = ref()
+const tasks = computed(()=> store.tasks)
+
+//Functions
+const handleCreate = async () =>{
+    try {
+        await store.addTask(taskTitle.value, userStore.user.id)
+        console.log(tasks)
+    } catch (error) {
+        console.log(error)
+    }
+    resetForm()
+}
+const resetForm = () => {
+    taskTitle.value = ""
+}
+
+
+//Lifecycle
+onMounted( async () => {
+    try {
+        await store.fetchTasks()
+        console.log(tasks)
+    } catch (error) {
+        console.log('error de llamada Fetch')
+        throw error
+    }
+})
+</script>
+
+<style scoped>
+main {
+    display: flex;
+    justify-content: space-between;
+}
+main div {
+    width: 47%;
+    background-color: #232323;
+    border-radius: 15px;
+    padding: 10px 20px;
+}
+ol {
+    list-style: circle;
+    margin-left: 20px;
+    margin-top: 20px;
+}
+.taskCard {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    background-color: #343434;
+    margin: 10px 0px;
+}
+
+button {
+    background-color: #F9F9F9;
+    padding: 5px 10px;
+    border: 0px;
+    border-radius: 15px;
+}
+form {
+    border: #F9F9F9 1px;
+    padding: 30px 0px
+}
+input {
+    margin-left: 10px;
+}
+</style>
--- src/stores/tasks.js
+++ src/stores/tasks.js
@@ -1,17 +1,28 @@
 import { defineStore } from 'pinia'
 import supabase from '../supabase/index'
 
-export const useTaskStore = defineStore('task',{
-    state: () => ({
-        tasks: null
-    }),
-    actions: {
-        async fetchTasks() {
-           const { data: tasks } = await supabase
-            .from('tasks')
-            .select("*")
-            .order('id', {ascending: false});
-           this.tasks = tasks;
-        }
+export const useTaskStore = defineStore('tasks', {
+  state: () => ({
+    tasks: null
+  }),
+  actions: {
+    async fetchTasks() {
+      const { data: tasks, error } = await supabase
+        .from('tasks')
+        .select()
+        .order('id', { ascending: false })
+      this.tasks = tasks
+      if (error) throw error
     },
-});
+    async addTask(title, user_id) {
+      const { data: [task], error } = await supabase
+      .from('tasks')
+      .insert({ title, user_id })
+      .select()
+      if (error) throw error
+      if(task) this.tasks.push(task)
+    },
+    async deleteTask() {},
+    async updateTask() {}
+  }
+})
